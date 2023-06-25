e6e5c44f9d7cacf19739441b75c2f377c29cdfe0
mandelakibiriti <mandelakibiriti@gmail.com>
2023-06-25 18:40:52 +0300

    changes to frontend and added docker implementation of ehrbase

--- editor/fastapi_server/app.py
+++ editor/fastapi_server/app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
 from .config import Settings as settings
-from .routers import api,db, fhir
+from .routers import api,db
 
 app = FastAPI()
 
